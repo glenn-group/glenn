7cd4b6e2f8ae9554c0a82b89df18705262356439
Jens Ljungblad <jens.ljungblad@gmail.com>
2011-03-03 14:06:44 +0100

    config merge updated

--- demos/helloworld/public/index.php
+++ demos/helloworld/public/index.php
@@ -8,20 +8,20 @@ spl_autoload_register(function($class_name) {
     require_once str_replace("\\", "/", $class_name) . '.php';
 });
 
-use glenn\controller\FrontController,
+use glenn\config\Config,
+	glenn\controller\FrontController,
 	glenn\http\Request;
-
-echo '<pre>';
-$config1 = new glenn\config\Config(include APP_PATH . '/config1.php');
-$config2 = new glenn\config\Config(include APP_PATH . '/config2.php');
-//print_r($config1);
-//print_r($config2);
-$config1 = glenn\config\Config::merge($config1, $config2);
-print_r($config1);
-exit;
 
 $request = new Request();
 $router = new glenn\router\RouterTree();
 $frontController = new FrontController($router);
 $response = $frontController->dispatch($request);
 $response->send();
+
+echo '<pre>';
+$config1 = new Config(include APP_PATH . '/config1.php');
+$config2 = new Config(include APP_PATH . '/config2.php');
+print_r($config1);
+print_r($config2);
+$config1->merge($config2);
+print_r($config1);
