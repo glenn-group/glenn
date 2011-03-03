09e9a7a130b30be5598eecf48b19ae0b8ac7313e
Erik Brännström <erik.brannstrom@streambur.se>
2011-03-03 21:26:54 +0100

    added event example to BlogController and made EventHandler methods static

--- demos/blog/app/controllers/BlogController.php
+++ demos/blog/app/controllers/BlogController.php
@@ -4,13 +4,27 @@ namespace controllers;
 use glenn\controller\Controller,
     glenn\http\Response,
     glenn\view\View,
-	models\Post;
+	models\Post,
+	glenn\event\EventHandler,
+	glenn\event\Event;
 
 class BlogController extends Controller {
+	
+	 public function __construct(\glenn\http\Request $request, \glenn\view\View $view)
+	 {
+		parent::__construct($request, $view);
+		EventHandler::bind('app.blog.index.post', function(Event $e) {
+			$e->subject()->view()->posts = Post::all();
+		});
+	 }
 
-    public function indexAction() 
-	{
-		$this->view->posts = Post::all();
+	 public function indexAction() 
+	 {
+		$obj = new \stdClass();
+		$obj->title = 'Title';
+		$obj->content = 'Content';
+		$this->view->posts = array($obj);
+		EventHandler::trigger(new Event($this, 'app.blog.index.post'));
     }
 
     public function newAction() 
