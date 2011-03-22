e3ce0a499c4acc93528499901545034830dfc9af
Erik Fonselius <fonsan@gmail.com>
2011-03-22 14:15:29 +0100

    Restructed repo

--- glenn/demos/blog/app/views/blog/new.php
+++
@@ -1,5 +0,0 @@
-<h1>Create new blog post</h1>
-
-<form action="/" method="post">
-    <input type="text" name="title" />
-</form>
--- views/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('classes/View.php');
-
-$myview = new View('testView');
-
-echo $myview;
-
-?>
