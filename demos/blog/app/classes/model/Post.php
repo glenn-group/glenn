b917d4a92bf1d748de635aa8fd9786696a688ff9
Erik Fonselius <fonsan@gmail.com>
2011-03-22 14:13:16 +0100

    Removed demos

--- demos/blog/app/classes/model/Post.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace app\model;
-
-class Post extends \ActiveRecord\Model 
-{
-    
-}
--- demos/helloworld/app/configs/config2.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-    'webhost'  => 'www.google.com',
-    'database' => array(
-        'adapter' => 'pdo_sqlite'
-    )
-);
