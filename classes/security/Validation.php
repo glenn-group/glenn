4223a64e6d7ea8aea9b4b0c7e39883395e615d52
Erik Brännström <erik.brannstrom@streambur.se>
2011-04-05 15:43:21 +0200

    Added a first implementation of a validation class.

---
+++ classes/security/Validation.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace glenn\security;
+
+/**
+ * Class containing static methods for validating data.
+ */
+class Validation
+{
+
+	/**
+	 * Return true if value is not null or an empty string.
+	 *
+	 * @param mixed $value
+	 * @return boolean 
+	 */
+	public static function required($value)
+	{
+		return $value !== null && $value !== '';
+	}
+	
+	/**
+	 * Return true if value contains more than the given amount of characters.
+	 *
+	 * @param mixed $value
+	 * @param integer $length
+	 * @return boolean 
+	 */
+	public static function lengthMin($value, $length)
+	{
+		return \strlen($value) > $length;
+	}
+	
+	/**
+	 * Return true if value contains less than the given amount of characters.
+	 *
+	 * @param mixed $value
+	 * @param integer $length
+	 * @return boolean 
+	 */
+	public static function lengthMax($value, $length)
+	{
+		return \strlen($value) < $length;
+	}
+	
+	/**
+	 * Return true if value contains exactly the given amount of characters.
+	 *
+	 * @param mixed $value
+	 * @param integer $length
+	 * @return boolean 
+	 */
+	public static function lengthEqual($value, $length)
+	{
+		return \strlen($value) === $length;
+	}
+	
+	/**
+	 * Return true if value contains only alphanumerical characters (a-zA-Z0-9).
+	 *
+	 * @param mixed $value
+	 * @return boolean 
+	 */
+	public static function alphanumeric($value)
+	{
+		return \ctype_alnum($value);
+	}
+	
+	/**
+	 * Return true if value only contains digits (0-9).
+	 *
+	 * @param mixed $value
+	 * @return boolean 
+	 */
+	public static function digit($value)
+	{
+		return \ctype_digit($value);
+	}
+	
+	/**
+	 * Return true if value contains only alphabetic characters of either case (a-zA-Z).
+	 *
+	 * @param mixed $value
+	 * @return boolean 
+	 */
+	public static function alpha($value)
+	{
+		return \ctype_alpha($value);
+	}
+	
+}
