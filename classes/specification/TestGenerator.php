f23a523eaf4952919a7658ae60724943e1f087e8
Peter Crona <p_crona@hotmail.com>
2011-04-14 19:17:05 +0200

    Added support for compiling to syntactically correct PHP. However, it ain't completly done yet. And will also need to build an environment in which the code is executed.

--- classes/specification/TestGenerator.php
+++ classes/specification/TestGenerator.php
@@ -6,48 +6,100 @@ namespace glenn\specification;
  *
  * @author peter
  */
-class TestGenerator {
+class TestGenerator
+{
 
 	private $annotation;
 	private $nrTests = 0;
+	private $tabLevel = 0;
+	private $codeBuffer = '';
 
-	public function __construct(Annotations $annotation) {
+	public function __construct(Annotations $annotation)
+	{
 		$this->annotation = $annotation;
 	}
 	
-	public function generateTest($class) {
+	public function generateTest($class)
+	{
 		$annotation = $this->annotation;
 		$specs = $annotation->getContracts($class);
 
 		// Create class which shall be filled with methods
-		$file = 'class Test'.$class."\n".'{'."\n";
+		$this->addCode('<?php'."\n", 0);
+		$this->addCode('class Test'.$class."\n".'{', 0);
 		
 		foreach($specs as $name => $spec) {
 			$tokenizer = new Parser\Tokenizer($spec);
 			$parser = new Parser\Parser($tokenizer);
 			$programModel = $parser->parseProgram();
 			
-			$file .= $this->compileToPHP($name, $programModel->interpret());
+			$this->compileToPHP($name, $programModel->interpret());
 		}
 
-		$file .= '}';
+		$this->addCode('}', 0);
+		
+		$dir = '../app/tests'.str_replace('\\', '/', substr($class, 0, strrpos($class, '\\')));
+		
+		if ( ! \file_exists($dir)) {
+			mkdir($dir, 0777, true);
+		}
 
 		// Write to file
-		echo $file;
+		return $this->flushBuffer('../app/tests'.str_replace('\\','/',$class).'.php');
 	}
 
-	private function compileToPHP($name, $code) {
+	private function compileToPHP($name, $code)
+	{
 		// Create method with test
 		$res = '';
 		foreach($code as $c) {
-			$res .= 'public function ' . $name . '_' . $c['type'] . '_' . $this->nrTests++ . " {\n" .print_r($c, true) . "}\n";
+			$this->addCode(
+				'public function ' . $name . '_' . $c['type'] . '_' . $this->nrTests++ . "\n{",
+				1
+			);
+
+			$this->addCode(
+				'if (' . $c['requires'] . ") { ",
+				2
+			);
+
+			$this->addCode(
+				'$this->assertTrue(' .
+				$c['ensures'] . ')',
+				3
+			);
+
+			$this->addCode(
+				'}',
+				2
+			);
+
+			$this->addCode(
+				'}'."\n",
+				1
+			);
 		}
 
 		return $res;
 	}
 
-	private function getClass($value) {
-		return substr($value, 0, strpos($value, '('));
+	private function addCode($code, $tabLevel)
+				{
+		$tabs = \str_repeat("\t", $tabLevel);
+		$code = $tabs . \str_replace("\n", "\n".$tabs, $code);
+		$this->buffer .= $code . "\n";
 	}
 
+	private function flushBuffer($file = null)
+	{
+		$code = $this->buffer;
+		$this->buffer = '';
+
+		if ($file == null) {
+			return $code;
+			
+		} else {
+			return \file_put_contents($file, $code);
+		}
+	}
 }
